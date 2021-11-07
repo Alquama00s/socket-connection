4a2c3d65f9fe7f13992e7a0ca4c53efdde118325
alquama00s <alquama00s@outlook.com>
2021-11-07 23:44:03 +0530

    init

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+// const httpserver = require('http').createServer(app);
+
+const server = app.listen(process.env.PORT || 3000, () => {
+	console.log('App Started at http://localhost:3000');
+});
+
+const io = require('socket.io')(server, {
+	cors: {
+		origin: '*',
+		methods: ['*'],
+	},
+});
+
+//const { instrument } = require('@socket.io/admin-ui');
+
+io.on('connection', socket => {
+	console.log(socket.id);
+
+	socket.on('key-pressed', (request, cb) => {
+		console.log(request);
+		setTimeout(() => {
+			const result = { value: request * 2 };
+			socket.emit('key-pressed',result);
+			//cb(result);
+		}, 3000);
+	});
+});
+
+app.get('/status', (req, res, next) => {
+	res.json({
+		status: 'OK',
+		date: `${new Date().toDateString()} ${new Date().toLocaleTimeString()}`,
+	});
+});
+
+// httpserver.listen(3000);
+
+//instrument(io, { auth: false });
